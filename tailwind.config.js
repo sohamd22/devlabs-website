7a1ae1d6c023f27868826eadd54bf304353959f7
Soham Daga <soham.daga22@gmail.com>
2024-06-21 11:30:48 +0530

    Changed layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
