ecbc6d5d53531ef86297b656bcad159b02da9933
Soham Daga <soham.daga22@gmail.com>
2024-06-28 09:11:06 +0530

    Configured for Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://sohamd22.github.io/devlabs-website",
+  base: "/devlabs-website"
 });
